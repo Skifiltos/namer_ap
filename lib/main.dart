b31cf168636a77a2136719aded6591749147adfb
badtype <g.c.faina@gmail.com>
2024-06-26 18:32:49 +0200

    aggiunta visualizzazione preferiti

--- lib/main.dart
+++ lib/main.dart
@@ -54,14 +54,43 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
+//mostra le parole preferite salvate dall'utente
+//al posto di placeholders di default
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
+
 //refactoring MyHomePage
 //trasferisco menù su barra laterale
 class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
-// ...
 
 class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
@@ -72,10 +101,9 @@ class _MyHomePageState extends State<MyHomePage> {
     switch (selectedIndex) {
       case 0:
         page = GeneratorPage();
-        break;
       case 1:
-        page = Placeholder();
-        break;
+        // page = Placeholder();
+        page = FavoritesPage();
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
